574c8d61f33d73de35e2471919e003f1ede73edb
Mangal81 <mangalkumar548744@gmail.com>
2024-08-24 22:30:56 +0530

    function added of lenth indicator value

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const slider = document.getElementById("range");
+const lengthIndicator = document.querySelector(".length-indicator");
+
+lengthIndicator.innerHTML = slider.value;
+
+slider.addEventListener("input",()=>{
+	const indicatorValue = slider.value;
+	lengthIndicator.innerHTML = indicatorValue;
+})
